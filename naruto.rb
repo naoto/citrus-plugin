c973932cc10c37fc91fae201512e1acd746ae573
Naoto SHINGAKI <nao.sora+github@gmail.com>
2010-05-24 23:05:49 +0900

    tab -> space

--- naruto.rb
+++ naruto.rb
@@ -2,26 +2,24 @@ require 'kconv'
 
 class Naruto < Citrus::Plugin
 
-	def on_privmsg(prefix, channel, message)
+  def on_privmsg(prefix, channel, message)
     
     if message =~ /(#{@config["words"]})/ &&
        (!@config["channels"] || @config["channels"].include?(channel))
 
       users = []
       @core.channels[channel][:modes].each { |mode, user|
-          users << user 
+        users << user 
       }
 
       diff = @core.channels[channel][:users] - users
       diff.each_slice(3) { |user|
-          post "mode #{channel} +ooo #{user.join(" ")}"
+        post "mode #{channel} +ooo #{user.join(" ")}"
       }
 
       msg = @config["reply"]
       notice channel, msg
 
     end
-
-	end
-
+  end
 end
