35bf7b22b4d3eb32a75db20be5cb14a1104300dc
Naoto SHINGAKI <nao.sora+github@gmail.com>
2010-05-24 23:03:20 +0900

    simple

--- naruto.rb
+++ naruto.rb
@@ -1,28 +1,23 @@
 require 'kconv'
-require 'yaml'
+
 class Naruto < Citrus::Plugin
+
 	def on_privmsg(prefix, channel, message)
     
     if message =~ /(#{@config["words"]})/ &&
        (!@config["channels"] || @config["channels"].include?(channel))
 
-      nouser = []
-      users = Hash.new()
-      postflg = false
+      users = []
       @core.channels[channel][:modes].each { |mode, user|
-          users[user] = true 
-      }
-      @core.channels[channel][:users].each { |user|
-        nouser << user if users[user].nil?
-        if nouser.length == 3
-          post "mode #{channel} +ooo #{nouser.join(" ")}"
-          nouser.clear
-          postflg = true
-        end
+          users << user 
       }
 
-      post "mode #{channel} +ooo #{nouser.join(" ")}" unless nouser.empty?
-      msg = (!postflg && nouser.empty?) ? @config["unreply"] : @config["reply"]
+      diff = @core.channels[channel][:users] - users
+      diff.each_slice(3) { |user|
+          post "mode #{channel} +ooo #{user.join(" ")}"
+      }
+
+      msg = @config["reply"]
       notice channel, msg
 
     end
